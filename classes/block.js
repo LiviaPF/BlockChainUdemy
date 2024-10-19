d572be19b36965e7d0e289b5fdb41da453c7c882
livia.pereira <liviap520@gmail.com>
2024-10-19 14:14:36 -0300

    feat: adiciona o atributo dificuldade na classe block

--- classes/block.js
+++ classes/block.js
@@ -1,11 +1,13 @@
 const SHA256 = require('crypto-js/sha256');
+const {DIFFICULTY} = require('../config');
 
 class Block {
-    constructor(timestamp, lastHash, hash, data) {
+    constructor(timestamp, lastHash, hash, data, nonce) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
     toString() {
@@ -13,23 +15,31 @@ class Block {
             timestamp = "${this.timestamp}"
             lastHash = "${this.lastHash}"
             hash = "${this.hash}"
-            data = "${this.data}"`;
+            data = "${this.data}"
+            nonce = "${this.nonce}"`;
     }
 
     static genesis() {
-        return new this('Genesis time', '', 'firstHash', []);
+        return new this('Genesis time', '', 'firstHash', [], 0);
     }
 
     static mineBlock (lastBlock, data) {
-        const timestamp = Date.now();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp, lastHash, data);
+        let nonce = 0;
 
-        return new this(timestamp, lastHash, hash, data);
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = Block.hash(timestamp, lastHash, data, nonce);
+        } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(this.DIFFICULTY));
+
+
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static hash (timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    static hash (timestamp, lastHash, data, nonce) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
     static blockHash (block) {
