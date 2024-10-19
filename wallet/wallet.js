7e48588c058c31f281ee63c44b81ddbef34a3fd0
livia.pereira <liviap520@gmail.com>
2024-10-19 15:47:01 -0300

    feat: altera valores dos atributos keypar e publicKey no construtor da classe Wallet

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -1,10 +1,11 @@
+const ChainUtil = require('../chain-util');
 const {INITIAL_BALANCE} = require('../config');
 
 class Wallet {
     constructor() {
         this.balance = INITIAL_BALANCE;
-        this.keypar = null;
-        this.publicKey = null;
+        this.keypar = ChainUtil.genKeyPair(); // gera uma chave pública e privada para o meu objeto
+        this.publicKey = this.keypar.getPublic().encode('hex');
     }
 
     toString() {
