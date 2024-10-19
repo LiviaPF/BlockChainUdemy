ea052cf6407a42bc0b9538b2f04ec6fc310d005d
livia.pereira <liviap520@gmail.com>
2024-10-19 10:44:35 -0300

    feat: cria método messageHandler e chama-o no método connectSocket, na classe P2pServer

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -41,6 +41,16 @@ class P2pServer {
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain)); // cada socket novo deve receber a cadeia de blocos atual
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
     }
 }
 
