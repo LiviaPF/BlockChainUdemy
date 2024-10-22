000dcf762dd89027c0a34ab6241902dbd43c020f
livia.pereira <liviap520@gmail.com>
2024-10-22 20:36:12 -0300

    feat: adiciona método para gerar e retornar id único na classe ChainUtil

--- chain-utils/chain-util.js
+++ chain-utils/chain-util.js
@@ -1,9 +1,14 @@
 const EC = require('elliptic').ec;
+const uuidV1 = require('uuid/v1'); // gera um id único, baseado no timestamp
 const ec = new EC('secp256k1'); // modelo de criptografia de curva elíptica utilizado pela Bitcoin
 
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+
+    static id() {
+        return uuidV1();
     }
 }
 
