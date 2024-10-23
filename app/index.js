1134b948b3a19e2501ab9855efeb3edd1f1af1e3
livia.pereira <liviap520@gmail.com>
2024-10-23 20:21:17 -0300

    feat: cria rota de post e get para transações na api

--- app/index.js
+++ app/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const Blockchain = require('../classes/blockchain');
 const HTTP_PORT = process.env.HTTP_PORT || 3001; // porta para rodar a aplicação
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet/wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const app = express();
 const bc = new Blockchain();
 const p2pServer = new P2pServer(bc);
+const tp= new TransactionPool();
+const wallet = new Wallet();
+
 
 app.use(express.json());
 app.get('/blocks', (req, res) => {
@@ -17,6 +22,16 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
     res.redirect('/blocks');
 })
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions)
+});
+
+app.post('/transact', (req, res) => {
+    const {recipient, amount} = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
