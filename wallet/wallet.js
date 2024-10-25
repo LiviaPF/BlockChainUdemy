8bc8cbee07929e65c9e04cdc032178d48658b5b1
livia.pereira <liviap520@gmail.com>
2024-10-25 17:30:47 -0300

    feat: cria método calculateBalance, na classe Carteira e o implementa no método createTransaction, da mesma classe

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -19,7 +19,8 @@ class Wallet {
         return this.keypar.sign(dataHash);
     }
 
-    createTransaction(recipient, amount, transactionPool) {
+    createTransaction(recipient, amount, blockchain, transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
         if (amount > this.balance) {
             console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
             return;
@@ -40,6 +41,42 @@ class Wallet {
         const blockchainWallet = new this();
         blockchainWallet.address = 'blockchain-wallet';
         return blockchainWallet;
+    }
+
+    calculateBalance(blockchain) {
+        let balance = this.balance; // variável balance armazena o saldo da carteira
+        let transactions = [];
+        blockchain.chain.forEach(block => block.data.forEach(transaction => {
+            transactions.push(transaction); // adiciona todas as transações do blockchain ao array transactions
+        }));
+
+        let startTime = 0;
+
+        // transações enviadas por essa carteira
+        const walletInputTs = transactions.filter(transaction => transaction.input.address === this.publicKey);
+
+        if (walletInputTs.length > 0) {
+            // reduz o array de transações até ter o elemento com o maior timestamp, que será a última transação
+            const recentInputTs = walletInputTs.reduce(
+                (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current, 0
+            );
+
+            // atualiza o saldo da carteira com o saldo restante após última transação enviada
+            balance = recentInputTs.outputs.find(output => output.address === this.publicKey).amount;
+            startTime = recentInputTs.input.timestamp;
+        }
+
+        transactions.forEach(transaction => {
+            if (transactions.input.timestamp > startTime) {
+                transaction.outputs.find(output => {
+                    if (output.address === this.publicKey) { // verifica nas transações, se a carteira é o destinatário
+                        balance += output.amount; // atualiza o saldo com o valor recebido na transação
+                    }
+                });
+            }
+        });
+
+        return balance;
     }
 }
 
