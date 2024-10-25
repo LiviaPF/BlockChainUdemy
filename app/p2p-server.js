948765170c62701c6fdc2a8c2e5c11902fa65812
livia.pereira <liviap520@gmail.com>
2024-10-25 17:33:38 -0300

    feat: cria método brodcastClearTransactions, adiciona MESSAGE_TYPE e seu case no switch case, tudo na classe P2pServer

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -9,8 +9,9 @@ const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 const MESSAGE_TYPES = {
     chain: 'CHAIN',
-    transaction: 'TRANSACTION'
-}
+    transaction: 'TRANSACTION',
+    clear_transactions: 'CLEAR_TRANSACTIONS'
+};
 
 class P2pServer {
     constructor(blockchain, transactionPool) {
@@ -63,6 +64,9 @@ class P2pServer {
                 case MESSAGE_TYPES.transaction:
                     this.transactionPool.updateOrAddTransaction(data.transaction);
                     break;
+                case MESSAGE_TYPES.clear_transactions:
+                    this.transactionPool.clear();
+                    break;
             }
         });
     }
@@ -81,6 +85,10 @@ class P2pServer {
 
     broadcastTransaction(transaction) {
         this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
+    }
+
+    broadcastClearTransactions() {
+        this.sockets.forEach(socket => socket.send(JSON.stringify({type: MESSAGE_TYPES.clear_transactions})));
     }
 }
 
